63286e90a02390e9511732b1b8bb9b3c39071006
ricrecar <75915201+ricrecar@users.noreply.github.com>
2024-06-09 16:17:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,12 @@ st.write(
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
-st.stop()
+
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
